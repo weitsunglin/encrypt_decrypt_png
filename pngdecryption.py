62a3b4c7e940facfc49116c26fa4d801bed73c7b
OLD_RD2林瑋琮 <wilson.lwt.tw@gmail.com>
2024-03-08 11:50:28 +0800

    Update pngdecryption.py

--- pngdecryption.py
+++ pngdecryption.py
@@ -6,32 +6,33 @@ _ENCRYSIG = b'PNG'  # 以bytes形式指定加密签名
 
 def decryption(fileData, key, encryS):
     """解密操作，实际上与加密操作相同，因为XOR是可逆的"""
-    # 首先移除加密签名
     if fileData.startswith(encryS):
-        fileData = fileData[len(encryS):]
+        fileData = fileData[len(encryS):]  # 移除加密签名
     else:
         print("The file does not start with the expected encryption signature.")
-        return fileData  # 返回未修改的数据
-
-    # 应用与加密相同的XOR逻辑进行解密
+        return None  # 如果文件不包含预期的签名，则返回None
     klen = len(key)
     decryptedData = bytearray(fileData)
     for i, byte in enumerate(decryptedData):
         decryptedData[i] = byte ^ key[i % klen]
     return decryptedData
 
-def process_decryption(filePath, key, encrySig):
-    """处理单个图片的解密"""
-    with open(filePath, 'rb') as file:
-        fileData = file.read()
-        decryptedData = decryption(fileData, key, encrySig)
-    with open(filePath, 'wb') as file:
-        file.write(decryptedData)
-    print(f"{os.path.basename(filePath)} has been decrypted.")
+def process_decryption(inputFilePath, outputFilePath, key, encrySig):
+    """处理单个图片的解密，并保存为新文件"""
+    if os.path.exists(inputFilePath):
+        with open(inputFilePath, 'rb') as file:
+            fileData = file.read()
+            decryptedData = decryption(fileData, key, encrySig)
+            if decryptedData is not None:
+                with open(outputFilePath, 'wb') as outFile:
+                    outFile.write(decryptedData)
+                print(f"{os.path.basename(inputFilePath)} has been decrypted and saved as {os.path.basename(outputFilePath)}.")
+            else:
+                print("Decryption failed. The file might not be encrypted with the expected signature.")
+    else:
+        print(f"{os.path.basename(inputFilePath)} does not exist.")
 
 if __name__ == "__main__":
-    decrypted_img_path = os.path.join(os.getcwd(), 'test_decrtpy.png')
-    if os.path.exists(decrypted_img_path):
-        process_decryption(decrypted_img_path, _KEY, _ENCRYSIG)
-    else:
-        print("Encrypted test.png does not exist in the script directory.")
+    encrypted_img_path = os.path.join(os.getcwd(), 'test_decrtpy.png')  # 假设加密的文件名为test.png
+    decrypted_img_path = os.path.join(os.getcwd(), 'test_encrypt.png')  # 解密后要保存的新文件名
+    process_decryption(encrypted_img_path, decrypted_img_path, _KEY, _ENCRYSIG)
